c10745f07a69d61386203bd3896d7cd7a583a47f
Nicolai <niha@nodes.dk>
2018-08-27 10:30:58 +0200

    first commit

---
+++ FirebaseCarthage/FirebaseCarthage.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseCarthage.h
+//  FirebaseCarthage
+//
+//  Created by Nicolai Harbo on 27/08/2018.
+//  Copyright © 2018 Nicolai Harbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirebaseCarthage.
+FOUNDATION_EXPORT double FirebaseCarthageVersionNumber;
+
+//! Project version string for FirebaseCarthage.
+FOUNDATION_EXPORT const unsigned char FirebaseCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseCarthage/PublicHeader.h>
+
+
